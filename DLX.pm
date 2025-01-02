44512041ef52cffafa3ed74598444b06619547c1
James M. Hammer, III <11713717+jmh0036@users.noreply.github.com>
2025-01-02 11:18:41 -0500

    Adding whitespace
    Whitespace for readability

--- DLX.pm
+++ DLX.pm
@@ -8,13 +8,13 @@ package DLX::Node;
 sub new {
     my ($class, $row, $col) = @_;
     my $self = {
-        row => $row,
-        col => $col,
-        left => undef,
-        right => undef,
-        up => undef,
-        down => undef,
-        column => undef,
+        row     => $row,
+        col     => $col,
+        left    => undef,
+        right   => undef,
+        up      => undef,
+        down    => undef,
+        column  => undef,
     };
     bless $self, $class;
     return $self;
@@ -26,8 +26,8 @@ use base 'DLX::Node';
 sub new {
     my ($class, $col) = @_;
     my $self = $class->SUPER::new(undef, $col);
-    $self->{size} = 0;
-    $self->{name} = $col;
+    $self->{size}   = 0;
+    $self->{name}   = $col;
     $self->{column} = $self;
     bless $self, $class;
     return $self;
@@ -39,57 +39,64 @@ package DLX;
 sub new {
     my ($class) = @_;
     my $self = {
-        header => DLX::Column->new('header'),
-        solution => [],
-        solutions => [],
+        header      => DLX::Column->new('header'),
+        solution    => [],
+        solutions   => [],
     };
+
     # Initialize header links
-    $self->{header}->{left} = $self->{header};
-    $self->{header}->{right} = $self->{header};
+    $self->{header}->{left}     = $self->{header};
+    $self->{header}->{right}    = $self->{header};
     bless $self, $class;
+
     return $self;
 }
 
 sub add_column {
     my ($self, $col_name) = @_;
     my $col = DLX::Column->new($col_name);
-    $col->{left} = $self->{header}->{left};
-    $col->{right} = $self->{header};
+
+    $col->{left}    = $self->{header}->{left};
+    $col->{right}   = $self->{header};
     $self->{header}->{left}->{right} = $col;
     $self->{header}->{left} = $col;
-    $col->{up} = $col;
-    $col->{down} = $col;
+    $col->{up}      = $col;
+    $col->{down}    = $col;
+
     return $col;
 }
 
 sub add_row {
     my ($self, $row, @cols) = @_;
     my $first;
+
     for my $col (@cols) {
         my $node = DLX::Node->new($row, $col->{name});
         $node->{column} = $col;
         $col->{size}++;
-        $node->{up} = $col->{up};
-        $node->{down} = $col;
-        $col->{up}->{down} = $node;
-        $col->{up} = $node;
+        $node->{up}         = $col->{up};
+        $node->{down}       = $col;
+        $col->{up}->{down}  = $node;
+        $col->{up}          = $node;
         if ($first) {
-            $node->{left} = $first->{left};
-            $node->{right} = $first;
+            $node->{left}   = $first->{left};
+            $node->{right}  = $first;
             $first->{left}->{right} = $node;
-            $first->{left} = $node;
+            $first->{left}  = $node;
         } else {
             $first = $node;
-            $node->{left} = $node;
-            $node->{right} = $node;
+            $node->{left}   = $node;
+            $node->{right}  = $node;
         }
     }
 }
 
 sub cover {
     my ($self, $col) = @_;
+
     $col->{right}->{left} = $col->{left};
     $col->{left}->{right} = $col->{right};
+
     for (my $row = $col->{down}; $row != $col; $row = $row->{down}) {
         for (my $node = $row->{right}; $node != $row; $node = $node->{right}) {
             $node->{down}->{up} = $node->{up};
@@ -101,6 +108,7 @@ sub cover {
 
 sub uncover {
     my ($self, $col) = @_;
+
     for (my $row = $col->{up}; $row != $col; $row = $row->{up}) {
         for (my $node = $row->{left}; $node != $row; $node = $node->{left}) {
             $node->{column}->{size}++;
@@ -108,20 +116,24 @@ sub uncover {
             $node->{up}->{down} = $node;
         }
     }
+
     $col->{right}->{left} = $col;
     $col->{left}->{right} = $col;
 }
 
 sub search {
     my ($self, $k) = @_;
+
     if ($self->{header}->{right} == $self->{header}) {
         push @{$self->{solutions}}, [@{$self->{solution}}];
         return;
     }
+
     my $col = $self->{header}->{right};
     for (my $c = $col->{right}; $c != $self->{header}; $c = $c->{right}) {
         $col = $c if $c->{size} < $col->{size};
     }
+
     $self->cover($col);
     for (my $row = $col->{down}; $row != $col; $row = $row->{down}) {
         push @{$self->{solution}}, $row->{row};
@@ -134,12 +146,15 @@ sub search {
         }
         pop @{$self->{solution}};
     }
+
     $self->uncover($col);
 }
 
 sub solve {
     my ($self) = @_;
+
     $self->search(0);
+
     return $self->{solutions};
 }
 
